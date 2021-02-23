eff98ddd84db8fa137d4ca18b9c0a8b43864816f
852098505 <42395062+852098505@users.noreply.github.com>
2021-02-23 13:37:13 +0800

    request请求头，request请求参数，request请求实体内容，request中POST请求乱码处理

---
+++ day08/src/main/java/cn/easymall/RegistServlet.java
@@ -0,0 +1,19 @@
+package cn.easymall;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class RegistServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //获取用户提交的请求参数
+        //校验数据
+        //检查用户名是否已经存在
+        //有问题提示
+        //没问题
+    }
+}
