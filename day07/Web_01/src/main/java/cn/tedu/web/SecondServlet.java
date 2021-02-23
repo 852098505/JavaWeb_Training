e1b15006c9c03682fa59d78b0ad395b5434ab8f3
852098505 <42395062+852098505@users.noreply.github.com>
2021-02-23 11:15:33 +0800

    easymall

--- day07/Web_01/src/main/java/cn/tedu/web/SecondServlet.java
+++ day07/Web_01/src/main/java/cn/tedu/web/SecondServlet.java
@@ -12,6 +12,7 @@ public class SecondServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String time = new Date().toLocaleString();
+        resp.setHeader("content-type","text/html;charset=utf-8");
         resp.getWriter().write(time + "");
     }
 }
